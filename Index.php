e7c121bc4c6f437dc63383210b6ae9f5d77a28a0
Julien <contact@jb-prod.fr>
2020-05-18 11:12:41 +0200

    index de base

---
+++ Index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/index.css">
+		<title>JB-prod.fr</title>
+	</head>
+	<body id="acceuil">
+		<header>
+			<H1>Bienvenue !</H1>
+		</header>
+		<section>
+			<div id="logo_acceuil"></div>
+		</section>
+		<footer>
+			<div><em>En construction<em></div>
+		</footer>
+	</body>
+</html>
